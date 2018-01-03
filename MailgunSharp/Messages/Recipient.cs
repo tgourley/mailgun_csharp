a5c28530d25aaad9dbe6d46191e4aa6eabb57ed8
Trey Gourley <trey.gourley@trustasc.com>
2018-01-03 14:38:45 -0500

    Added MailgunSharp project that compiles to .Net Standard 2.0.  Copied class files from main Mailgun project.

---
+++ MailgunSharp/Messages/Recipient.cs
@@ -0,0 +1,54 @@
+﻿using Mailgun.Core.Messages;
+using Mailgun.Exceptions;
+
+namespace Mailgun.Messages
+{
+    /// <summary>
+    /// A structure that represents an email recipient with a DisplayName and an e-mail address
+    /// </summary>
+    public struct Recipient : IRecipient
+    {
+        /// <summary>
+        /// The display name for the recipient
+        /// </summary>
+        public string DisplayName { get; set; }
+
+        /// <summary>
+        /// The email address for the recipient
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Get this recipients value as a formatted string. For example, if a display name is provided, it will return 'displayName <address>'
+        /// but if no display name is provided, it will return 'address'
+        /// </summary>
+        /// <returns></returns>
+        public string ToFormattedString()
+        {
+            ValidateEmail();
+            var displayName = DisplayName ?? "";
+            //Escape quotes in display name
+            displayName = displayName.Replace("\"", "\\\"");
+            return displayName == ""
+                ? Email
+                : $"\"{displayName}\" <{Email}>";
+        }
+
+        /// <summary>
+        /// That's just some basic checks to handle already happened situations. Mailgun doesn't signal you
+        /// specific failing emails (and it can be hundreds of them in one batch in some scenarios).
+        /// Better check something on client side
+        /// </summary>
+        private void ValidateEmail()
+        {
+            if (!Email.Contains("@"))
+            {
+                throw new InvalidEmailException(Email);
+            }
+            if (Email.Contains(".@"))
+            {
+                throw new InvalidEmailException(Email);
+            }
+        }
+    }
+}
---
+++ MailgunSharp/Service/MessageService.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Mailgun.Core.Messages;
+using Mailgun.Core.Service;
+using Mailgun.Exceptions;
+
+namespace Mailgun.Service
+{
+    /// <summary>
+    /// The core mailgun service
+    /// </summary>
+    public class MessageService : IMessageService
+    {
+        public string ApiKey { get; private set; }
+        public string BaseAddress { get; private set; }
+        public bool UseSSl { get; private set; }
+
+        /// <summary>
+        /// Create an instance of the mailgun service with the specified apikey
+        /// </summary>
+        /// <param name="apikey">Your mailgun API Key</param>
+        /// <param name="useSsl">Should the library use SSL for all requests?</param>
+        /// <param name="baseAddress">Base address of the mailgun api, excluding the scheme, e.g. api.mailgun.net/v3</param>
+        public MessageService(string apikey, bool useSsl = true, string baseAddress = "api.mailgun.net/v3")
+        {
+            ApiKey = apikey;
+            BaseAddress = baseAddress;
+            UseSSl = useSsl;
+        }
+
+        /// <summary>
+        /// Send a message using the Mailgun API service.
+        /// </summary>
+        /// <param name="workingDomain">The mailgun working domain to use</param>
+        /// <param name="message">The message to send</param>
+        /// <returns></returns>
+        public async Task<HttpResponseMessage> SendMessageAsync(string workingDomain, IMessage message)
+        {
+            //check for parameters
+            ThrowIf.IsArgumentNull(() => workingDomain);
+            ThrowIf.IsArgumentNull(() => message);
+
+
+            //build request
+            using (var client = new HttpClient())
+            {
+                var buildUri = new UriBuilder
+                {
+                    Host = BaseAddress,
+                    Scheme = UseSSl ? "https" : "http",
+                    Path = string.Format("{0}/messages", workingDomain)
+                };
+
+
+                //add authentication
+                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic",
+                    Convert.ToBase64String(Encoding.UTF8.GetBytes("api:" + ApiKey)));
+
+
+                //set the client uri
+                return await client.PostAsync(buildUri.ToString(), message.AsFormContent());
+                //TODO may be read answer and throw exceptions on errors.
+                //or introduce some new API that will do that
+            }
+        }
+    }
+}
